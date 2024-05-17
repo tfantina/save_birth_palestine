20ddf32c80ca9ab87b5348ef0dfccc2eec4a9fde
Travis Fantina <tfantina@gmail.com>
2024-05-17 08:39:04 -0400

    Initializes Svelte project

---
+++ .vercel/output/functions/fn.func/.svelte-kit/output/server/entries/pages/_page.svelte.js
@@ -0,0 +1,8 @@
+import { c as create_ssr_component } from "../../chunks/ssr.js";
+const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
+  return `<div class="f-row section body-landing" data-svelte-h="svelte-1pvnrrd"><div class="col-3"><img src="/images/logo.png"></div> <div class="f-col col-9"><h1>Safe Birth In Palestine</h1> <h2>Comitted to providing support for the 50,000 pregnant women in Gaza</h2> <p>A coalition of childbirth professionals for supporting mothers and babies in Gaza, Palestine. CEASEFIRE NOW!</p> <h3 class="pt-l"><em>As featrued in</em></h3> <article class="lede-card pt-2xs"><a href="https://www.theguardian.com/world/2024/feb/07/gaza-pregnant-women-safe-birth-in-palestine-project"><div class="f-row justify-between"><div class="lede-card__content self-center"><h4 class="pb-xs">The Guardian</h4>
+                    Thus far, Safe Birth in Palestine Project has provided Arabic language videos that give emergency unassisted birth instructions, and it is collaborating with local midwives on the development of an antenatal clinic... (read more)</div> <div class="lede-card__img"><img src="/images/guardian.avif"></div></div></a></article></div></div>`;
+});
+export {
+  Page as default
+};
---
+++ svelte.config.js
@@ -0,0 +1,15 @@
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from '@sveltejs/adapter-vercel';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	preprocess: [vitePreprocess({})],
+
+	kit: {
+		adapter: adapter({ runtime: 'nodejs18.x' })
+	}
+};
+
+export default config;
